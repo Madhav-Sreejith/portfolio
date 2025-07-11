834620012def5ce00f39fc5b75fe408c415238d1
Madhav <madhavsreejith8590@gmail.com>
2025-07-11 18:40:12 +0530

    upadate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Project from './pages/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>Home</Link>
+        <Link to="/about" style={{ padding: 5 }}>About</Link>
+        <Link to="/project" style={{ padding: 5 }}>Projects</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Project.jsx
@@ -0,0 +1,11 @@
+export default function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <ul>
+        <li>Portfolio Website</li>
+        <li>Todo App</li>
+      </ul>
+    </div>
+  );
+}
