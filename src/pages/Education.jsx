d920322d189f6eaa456fd9869f9be5dfd2a03f60
Madhav <madhavsreejith8590@gmail.com>
2025-07-25 00:03:47 +0530

    educationbox

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -8,21 +8,25 @@ function Education() {
 
       <div className="education-grid">
         <div className="education-box">
-          <img src="/amritalogo.jpeg" alt="College Logo" className="institution-logo" />
+            <div className="logo-container">
+                 <img src="/amritalogo.jpeg" alt="College Logo" className="institution-logo" />
+            </div>
           <div className="education-content">
-            <h3>College</h3>
             <p>Amrita Vishwa Vidyapeetham</p>
-            <p>B.Tech in Computer Science</p>
-            <p>2023 - 2027</p>
+            <p>Bachelor of Technology- Computer Science</p>
+            <p>2023-Present</p>
           </div>
         </div>
 
         <div className="education-box">
-          <img src="/rijuandpsk.png" alt="School Logo" className="institution-logo" />
+            <div className="logo-container">
+                <img src="/rijuandpsk.png" alt="School Logo" className="institution-logo" />
+            </div>
           <div className="education-content">
-            <h3>School</h3>
-            <p>ABC Higher Secondary School</p>
-            <p>12th Grade - 2021</p>
+            <p>Gurukula Public School</p>
+            <p>Grade: 96%</p>
+            <p>2021-2023</p>
+
           </div>
         </div>
       </div>
